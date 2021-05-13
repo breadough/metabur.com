6c19bb8c5d8f8d436e7cd4b0d83b7b4f7572d36d
breadough <brdllovido@gmail.com>
2021-05-13 13:44:13 +0800

    testing sign up and sign in function

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Metabur</title>
+  </head>
+  <body>
+      <div class="container">
+        <h1>Sign Up</h1>
+        <p>Please fill in this form to create an account.</p>
+
+        <label><b>User Name</b></label>
+        <input type="text" placeholder="Username" required>
+        
+        <label><b>Email</b></label>
+        <input type="text" placeholder="Email" required>
+ 
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Password" required>
+    
+        <label><b>Repeat Password</b></label>
+        <input type="password" placeholder="Password" required>
+        
+        <p>By creating an account you agree to our Terms & Privacy</p>
+    
+        <div>
+          <button type="submit" class="signupbtn">Sign Up</button>
+          <button type="button" class="cancelbtn">Cancel</button>
+        </div>
+      </div>
+  </body>
+</html>
